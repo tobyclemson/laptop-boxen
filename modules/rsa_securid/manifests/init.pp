7d5579461c068c470a6de1a3148e38cb107cf260
Toby Clemson <tobyclemson@gmail.com>
2013-09-15 22:46:40 -0300

    Toby - Oops forgot name for RSA package.

--- modules/rsa_securid/manifests/init.pp
+++ modules/rsa_securid/manifests/init.pp
@@ -1,6 +1,7 @@
 class rsa_securid {
-  package {
+  package { 'RSA SecurID':
     provider => 'pkgdmg',
-    source => "ftp://ftp.rsasecurity.com/pub/agents/RSASecurIDMac412.dmg"
+    source => "ftp://ftp.rsasecurity.com/pub/agents/RSASecurIDMac412.dmg",
+    ensure => 'installed'
   }
 }
