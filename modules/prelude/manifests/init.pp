8b79cf0dcd7e5c4d452c5fe2577a5686d906ed90
Toby Clemson <tobyclemson@gmail.com>
2013-09-15 21:31:58 -0300

    Toby - Installing aspell along with prelude to satisfy flyspell.

--- modules/prelude/manifests/init.pp
+++ modules/prelude/manifests/init.pp
@@ -1,6 +1,9 @@
 class prelude {
   $home_dir = "/Users/${::luser}"
 
+  # emacs flyspell uses aspell
+  package { 'aspell': }
+
   repository { "$home_dir/.emacs.d":
     source   => 'bbatsov/prelude',
     provider => 'git',
