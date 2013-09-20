60a0baefa7ea6c34a23cbac392cb8e3ad73e58d0
Toby Clemson <tobyclemson@gmail.com>
2013-09-20 17:28:16 -0300

    Toby - Checking hiera config by dropping identity file into home directory.

--- modules/rsa_securid/manifests/init.pp
+++ modules/rsa_securid/manifests/init.pp
@@ -1,7 +1,19 @@
-class rsa_securid {
+class rsa_securid($identity) {
+  $home_dir = "/Users/${::luser}"
+
   package { 'RSA SecurID':
     provider => 'pkgdmg',
     source => "ftp://ftp.rsasecurity.com/pub/agents/RSASecurIDMac412.dmg",
     ensure => 'installed'
+  }
+
+  file { '$home_dir/tclemson-identity.sdtid':
+    content => $identity,
+    ensure => 'present'
+  }
+
+  file { '$home_dir/boxen_home':
+    content => $::boxen_home,
+    ensure => 'present'
   }
 }
