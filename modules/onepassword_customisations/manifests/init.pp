31231de41421daa3f80d38c24749d5ce417b6ed2
Toby Clemson <tobyclemson@gmail.com>
2013-09-28 20:54:18 -0300

    Adding missing type information in onepassword license installation.

--- modules/onepassword_customisations/manifests/init.pp
+++ modules/onepassword_customisations/manifests/init.pp
@@ -7,12 +7,14 @@ class onepassword_customisations(
   plistbuddy::add { 'Install 1Password license person':
     target_plist => "${home_dir}/Library/Preferences/ws.agile.1Password.plist",
     add_path => ":License:Person",
+    type => 'string',
     value => $person,
   }
 
   plistbuddy::add { 'Install 1Password license serial':
     target_plist => "${home_dir}/Library/Preferences/ws.agile.1Password.plist",
     add_path => ":License:Serial",
+    type => 'string',
     value => $serial,
   }
 
