bd151e83512e53f6f1389879fb77bb7c4f9a1336
Toby Clemson <tobyclemson@gmail.com>
2013-09-28 20:46:53 -0300

    Using plistbuddy set to install onepassword license file.

--- modules/onepassword_customisations/manifests/init.pp
+++ modules/onepassword_customisations/manifests/init.pp
@@ -4,6 +4,19 @@ class onepassword_customisations(
 ) {
   require onepassword
 
+  plistbuddy::set { 'Install 1Password license person':
+    target_plist => "${home_dir}/Library/Preferences/ws.agile.1Password.plist",
+    set_path => ":License:Person",
+    value => $person,
+  }
+
+  plistbuddy::set { 'Install 1Password license serial':
+    target_plist => "${home_dir}/Library/Preferences/ws.agile.1Password.plist",
+    set_path => ":License:Serial",
+    value => $serial,
+  }
+
+
   # boxen::osx_defaults { 'Install 1Password license person':
   #   key => "License:Person",
   #   value => $person,
---
+++ modules/plistbuddy/manifests/set.pp
@@ -0,0 +1,22 @@
+define plistbuddy::set(
+  $target_plist = undef,
+  $set_path = undef,
+  $value = undef
+) {
+  $plistbuddy_cmd = '/usr/libexec/PlistBuddy'
+
+  if ($target_plist == undef) or
+     ($set_path == undef) or
+     ($value == undef) {
+    fail('Cannot set value in plist without target_plist, set_path and value attributes')
+  }
+
+  $set_cmd = "$plistbuddy_cmd -c \"Set $set_path $value\" $target_plist"
+  $current_hash_cmd = "$plistbuddy_cmd -x -c \"Print $set_path\" $target_plist | md5"
+  $new_hash_cmd = "echo \"$value\" | md5"
+
+  exec { $merge_cmd:
+    require => File[$plist_tmp_file],
+    unless => "echo \"[ \\\"\$($current_hash_cmd)\\\" == \\\"\$($new_hash_cmd)\\\" ]\" | bash"
+  }
+}
