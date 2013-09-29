56fd41764003b9913ab62d14b8e643b767188df2
Toby Clemson <tobyclemson@gmail.com>
2013-09-28 21:51:35 -0300

    Fixing paths in onepassword license plist manipulation.

--- modules/onepassword_customisations/manifests/init.pp
+++ modules/onepassword_customisations/manifests/init.pp
@@ -4,36 +4,19 @@ class onepassword_customisations(
 ) {
   require onepassword
 
+  $home_dir = "/Users/${::boxen_user}"
+
   plistbuddy::add { 'Install 1Password license person':
     target_plist => "${home_dir}/Library/Preferences/ws.agile.1Password.plist",
-    add_path => ":License:Person",
+    add_path => ":'License':'Person'",
     type => 'string',
     value => $person,
   }
 
   plistbuddy::add { 'Install 1Password license serial':
     target_plist => "${home_dir}/Library/Preferences/ws.agile.1Password.plist",
-    add_path => ":License:Serial",
+    add_path => ":'License':'Serial'",
     type => 'string',
     value => $serial,
   }
-
-
-  # boxen::osx_defaults { 'Install 1Password license person':
-  #   key => "License:Person",
-  #   value => $person,
-  #   domain => 'ws.agile.1Password',
-  #   user => $::boxen_user,
-  #   ensure => 'present',
-  #   require => Class['onepassword']
-  # }
-
-  # boxen::osx_defaults { 'Install 1Password license serial':
-  #   key => "License:Serial",
-  #   value => $serial,
-  #   domain => 'ws.agile.1Password',
-  #   user => $::boxen_user,
-  #   ensure => 'present',
-  #   require => Class['onepassword']
-  # }
 }
