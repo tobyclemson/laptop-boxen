236a82bda9827741058412621bce40c7eba9b89c
Toby Clemson <tobyclemson@gmail.com>
2013-09-28 22:45:32 -0300

    Fixing bad dependencies in terminal customisations.

--- modules/terminal_customisations/manifests/init.pp
+++ modules/terminal_customisations/manifests/init.pp
@@ -1,7 +1,7 @@
 class terminal_customisations {
   $home_dir = "/Users/${::boxen_user}"
 
-  plist::contents { 'Add IR_Black profile':
+  plist::contents { 'IR_Black profile':
     source => "puppet:///modules/terminal_customisations/ir_black.profile.plist",
     target => "${home_dir}/Library/Preferences/com.apple.Terminal.plist",
     path => ":'Window Settings'",
@@ -14,7 +14,7 @@ class terminal_customisations {
     domain => 'com.apple.Terminal',
     user => $::boxen_user,
     ensure => 'present',
-    require => Plistbuddy::Merge['Add IR_Black profile']
+    require => Plist::Contents['IR_Black profile']
   }
 
   boxen::osx_defaults { 'Use IR_Black by default':
@@ -23,6 +23,6 @@ class terminal_customisations {
     domain => 'com.apple.Terminal',
     user => $::boxen_user,
     ensure => 'present',
-    require => Plistbuddy::Merge['Add IR_Black profile']
+    require => Plist::Contents['IR_Black profile']
   }
 }
