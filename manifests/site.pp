973835d09da1cb1665a54292e079efc6cb57584e
Toby Clemson <tobyclemson@gmail.com>
2014-06-14 19:09:27 -0400

    Add local to search path.

--- manifests/site.pp
+++ manifests/site.pp
@@ -12,6 +12,7 @@ Exec {
     "${boxen::config::home}/rbenv/bin",
     "${boxen::config::home}/rbenv/plugins/ruby-build/bin",
     "${boxen::config::home}/homebrew/bin",
+    '/usr/local/bin',
     '/usr/bin',
     '/bin',
     '/usr/sbin',
