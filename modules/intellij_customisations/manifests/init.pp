3ef758e439b4ed84959fa00fc29ed06233d52be3
Toby Clemson <tobyclemson@gmail.com>
2014-06-15 22:45:30 -0400

    Ensure directory exists in intellij customisations module.

--- modules/intellij_customisations/manifests/init.pp
+++ modules/intellij_customisations/manifests/init.pp
@@ -5,13 +5,19 @@ class intellij_customisations(
 
   $home_dir = "/Users/$::boxen_user"
 
+  file { "$home_dir/Library/Preferences/IntelliJIdea13":
+    ensure => 'directory',
+  }
+
   file { "$home_dir/Library/Preferences/IntelliJIdea13/idea13.key":
     ensure => 'present',
-    content => $license
+    content => $license,
+    require => File["$home_dir/Library/Preferences/IntelliJIdea13"],
   }
 
   file { "$home_dir/Library/Preferences/IntelliJIdea13/disabled_plugins.txt":
     ensure => 'present',
-    source => "puppet:///modules/intellij_customisations/disabled_plugins.txt"
+    source => "puppet:///modules/intellij_customisations/disabled_plugins.txt",
+    require => File["$home_dir/Library/Preferences/IntelliJIdea13"],
   }
 }
