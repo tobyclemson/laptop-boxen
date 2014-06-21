df57b0d06ca7dde9417f757f8ab773bcdb7f3a16
Toby Clemson <tobyclemson@gmail.com>
2014-06-21 15:13:48 -0400

    Copy kernel extension using sudo.

--- modules/s3storage/manifests/init.pp
+++ modules/s3storage/manifests/init.pp
@@ -2,12 +2,12 @@ class s3storage {
   package { 's3fs': }
 
   exec { 'copy-fuse4x':
-    command => 'cp -rfX /opt/boxen/homebrew/Cellar/fuse4x-kext/0.9.2/Library/Extensions/fuse4x.kext /Library/Extensions',
+    command => 'sudo /bin/cp -rfX /opt/boxen/homebrew/Cellar/fuse4x-kext/0.9.2/Library/Extensions/fuse4x.kext /Library/Extensions',
     require => Package['s3fs'],
     creates => '/Library/Extensions/fuse4x.kext',
   }
 
-  exec { 'chmod +s /Library/Extensions/fuse4x.kext/Support/load_fuse4x':
+  exec { 'sudo /bin/chmod +s /Library/Extensions/fuse4x.kext/Support/load_fuse4x':
     unless  => "ls -l /Library/Extensions/fuse4x.kext/Support/load_fuse4x | cut -d' ' -f 1 | grep 's' 2>/dev/null",
     require => [
       Package['s3fs'],
