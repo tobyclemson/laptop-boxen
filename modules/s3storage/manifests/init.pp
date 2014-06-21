29079a725be421473228b3dd00db2251b4f899da
Toby Clemson <tobyclemson@gmail.com>
2014-06-21 15:10:43 -0400

    Copy kernel extension for s3fs.

--- modules/s3storage/manifests/init.pp
+++ modules/s3storage/manifests/init.pp
@@ -1,3 +1,17 @@
 class s3storage {
   package { 's3fs': }
+
+  exec { 'copy-fuse4x':
+    command => 'cp -rfX /opt/boxen/homebrew/Cellar/fuse4x-kext/0.9.2/Library/Extensions/fuse4x.kext /Library/Extensions',
+    require => Package['s3fs'],
+    creates => '/Library/Extensions/fuse4x.kext',
+  }
+
+  exec { 'chmod +s /Library/Extensions/fuse4x.kext/Support/load_fuse4x':
+    unless  => "ls -l /Library/Extensions/fuse4x.kext/Support/load_fuse4x | cut -d' ' -f 1 | grep 's' 2>/dev/null",
+    require => [
+      Package['s3fs'],
+      Exec['copy-fuse4x']
+    ],
+  }
 }
