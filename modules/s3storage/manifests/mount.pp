c4301446377c449aaac0251fb8d36dd7e0fdbc7a
Toby Clemson <tobyclemson@gmail.com>
2014-06-21 23:10:39 -0400

    Extract s3storage mount command into template.

--- modules/s3storage/manifests/mount.pp
+++ modules/s3storage/manifests/mount.pp
@@ -3,6 +3,8 @@ define s3storage::mount($aws_account, $root) {
   $cache_dir = "/tmp/s3storage-cache-${title}"
   $passwd_file = "${root}/s3storage-${aws_account}.creds"
 
+  $mount_command = template('s3storage/mount.erb')
+
   file { $mount_dir:
     ensure  => 'directory',
     owner   => 'root',
@@ -21,14 +23,12 @@ define s3storage::mount($aws_account, $root) {
   sudoers { "s3fs-${aws_account}-mount":
     users    => $::boxen_user,
     hosts    => 'ALL',
-    commands => [
-                 "(ALL) NOPASSWD : /opt/boxen/homebrew/bin/s3fs ${title} ${mount_dir} -o passwd_file=${passwd_file} -o allow_other -o use_cache=${cache_dir} -o connect_timeout=3600 -o readwrite_timeout=7200 -o daemon_timeout=14400"
-    ],
+    commands => ["(ALL) NOPASSWD : ${mount_command}"],
     type     => 'user_spec',
   }
 
   exec { "mount ${title}":
-    command => "sudo /opt/boxen/homebrew/bin/s3fs ${title} ${mount_dir} -o passwd_file=${passwd_file} -o allow_other -o use_cache=${cache_dir} -o connect_timeout=3600 -o readwrite_timeout=7200 -o daemon_timeout=14400",
+    command => $mount_command,
     unless  => "test -n \"`/bin/df ${mount_dir} | awk '/s3fs/ { print \$1 }'`\"",
     require => [
       File[$mount_dir],
