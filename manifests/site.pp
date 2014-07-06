1c13155658e7816679a37f18f10c7f1555c54d06
Toby Clemson <tobyclemson@gmail.com>
2014-07-06 17:29:04 -0300

    Set default node version.

--- manifests/site.pp
+++ manifests/site.pp
@@ -133,6 +133,10 @@ node default {
   include nodejs::v0_8
   include nodejs::v0_10
 
+  class { 'nodejs::global':
+    version => 'v0.10'
+  }
+
   ruby::version { '1.9.3': }
   ruby::version { '2.0.0': }
   ruby::version { '2.1.0': }
