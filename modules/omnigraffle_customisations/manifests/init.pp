bfbb780983a6bb62be101ebe448ed0ffcf3a07b4
Toby Clemson <tobyclemson@gmail.com>
2014-07-06 20:49:06 -0300

    Update omnigraffle license location.

--- modules/omnigraffle_customisations/manifests/init.pp
+++ modules/omnigraffle_customisations/manifests/init.pp
@@ -3,14 +3,27 @@ class omnigraffle_customisations(
   $owner
 ) {
   $username = $::boxen_user
-  $omni_dir = "/Users/${username}/Library/Application Support/Omni Group"
+  $containers_dir = "/Users/${username}/Library/Containers"
+  $omni_container_dir = "${containers_dir}/com.omnigroup.OmniGraffle6"
+  $data_dir = "${omni_container_dir}/Data"
+  $library_dir = "${data_dir}/Library"
+  $app_support_dir = "${library_dir}/Application Support"
+  $omni_dir = "${app_support_dir}/Omni Group"
   $license_dir = "${omni_dir}/Software Licenses"
 
-  file { [$omni_dir, $license_dir]:
+  file { [
+      $containers_dir,
+      $omni_container_dir,
+      $data_dir,
+      $library_dir,
+      $app_support_dir,
+      $omni_dir,
+      $license_dir
+    ]:
     ensure => 'directory',
   }
 
-  file { "${license_dir}/Omnigraffle-503655.omnilicense":
+  file { "${license_dir}/Omnigraffle-762283.omnilicense":
     ensure  => 'present',
     content => template("omnigraffle_customisations/license.plist.erb"),
   }
